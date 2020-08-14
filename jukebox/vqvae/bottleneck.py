c50795baa6b733f8726012722edfec28e5e872cf
DorTenen <dort@wix.com>
2020-08-14 15:00:15 +0300

    Delete redundant print

--- jukebox/vqvae/bottleneck.py
+++ jukebox/vqvae/bottleneck.py
@@ -165,8 +165,6 @@ class BottleneckBlock(nn.Module):
         if update_k and not self.init:
             self.init_k(x)
         self.k = self.get_cur_embeddings()
-        print_all(self.base)
-        print_all(self.k)
 
         # Quantise and dequantise through bottleneck
         x_l, fit = self.quantise(x)
